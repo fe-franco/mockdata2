19ecdb67fc4dc4807e4f793d8de3bd7550892fbe
Felipe <felipe.francc2@gmail.com>
2023-11-02 16:30:47 -0300

    insert 1000

--- src/sql_generator.rs
+++ src/sql_generator.rs
@@ -77,7 +77,7 @@ impl<T> SqlGenerator<T> {
         let file = File::create(&filename)?;
         let mut writer = BufWriter::new(file);
 
-        for chunk in self.data.chunks(500) {
+        for chunk in self.data.chunks(1000) {
             writer.write_all(b"INSERT ALL\n")?;
             for item in chunk {
                 let sql = item.to_insert_sql(table_name);
